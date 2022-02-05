73444a83f1a7b8568c569ad9193687f1fb32aa82
Фаиз <galimow63@gmail.com>
2022-02-05 12:42:21 +0400

    Passing data through pages

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen();
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Recipes"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text("The Recipes For The Category"),
