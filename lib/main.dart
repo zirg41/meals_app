c331be53fd5c961c099f089d62262201d742948b
Фаиз <galimow63@gmail.com>
2022-02-11 19:45:01 +0400

    _favoriteMeals empty list added

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
   };
 
   List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
   void _setFilters(Map<String, bool> filterData) {
     setState(() {
