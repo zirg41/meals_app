3b3a8125fc19715d78990cd97f100108896bd8b6
Фаиз <galimow63@gmail.com>
2022-02-11 19:45:25 +0400

    changing no favotites text

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -6,7 +6,7 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Favorites!"),
+      child: Text("You have no favorites yet - start adding some!"),
     );
   }
 }
